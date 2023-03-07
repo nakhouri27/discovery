fb3ce96189150a76d148a0654e43aa10eafa5e30
Nishant Akhouri <nishant.akhouri@Apexon.com>
2023-03-07 12:03:09 +0530

    wip: added microservices

---
+++ src/test/java/com/okta/developer/docker_microservices/discovery/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.developer.docker_microservices.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
